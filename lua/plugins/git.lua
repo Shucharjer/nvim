0355ea497e96ac0de5a9bce64323d3d1a65bbb8b
Shucharjer <Shucharjer@outlook.com>
2025-04-29 22:24:14 +0800

    add tui for git
    add tui support for git via lazygit, users should install lazygit if
    they want to use it

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -10,4 +10,27 @@ return {
     {
         "airblade/vim-gitgutter"
     },
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>g",  group = "Git" },
+            { "<leader>gg", "<cmd>LazyGit<cr>",       desc = "LazyGit" },
+            { "<leader>gc", "<cmd>LazyGitConfig<cr>", desc = "LazyGit config" },
+            { "<leader>gf", "<cmd>LazyGitFilter<cr>", desc = "LazyGit filter" }
+        }
+    }
 }
